19498d2782dafd8afcf8e3f2150131333c6a4184
duyllyan <duyllyan@gmail.com>
2025-04-12 17:15:44 -0300

    Initial Commit

---
+++ RabbitMaze/RabbitMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RabbitMazeApp.swift
+//  RabbitMaze
+//
+//  Created by Duyllyan Almeida de Carvalho on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RabbitMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RabbitMazeTests/RabbitMazeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RabbitMazeTests.swift
+//  RabbitMazeTests
+//
+//  Created by Duyllyan Almeida de Carvalho on 12/04/25.
+//
+
+import Testing
+@testable import RabbitMaze
+
+struct RabbitMazeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
